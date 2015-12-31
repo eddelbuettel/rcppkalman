136eede31112589e7229f7ac18072b48f72bdf2f
Dirk Eddelbuettel <edd@debian.org>
2015-12-31 09:58:54 -0600

    general polishing in a few place to make R-devel CMD check real happy

--- src/expmMat.cpp
+++ src/expmMat.cpp
@@ -34,7 +34,8 @@ extern "C" void R_init_RcppKalman(DllInfo *dll) {
 //' M <- matrix(c(4, 1, 1, 2, 4, 1, 0, 1, 4), 3, 3)
 //' 
 //' ## expected output
-//' expM <- matrix(c(147.8666, 127.7811, 127.7811, 183.7651, 183.7651, 163.6796, 71.79703, 91.88257, 111.96811), 3, 3)
+//' expM <- matrix(c(147.8666, 127.7811, 127.7811, 183.7651, 183.7651,
+//'                  163.6796, 71.79703, 91.88257, 111.96811), 3, 3)
 //' 
 //' ## we only have the expected result to about six digits
 //' all.equal(expm(M), expM, tolerance=1.0e-6)
