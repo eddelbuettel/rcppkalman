5a568f1e2a79412e1be35e91f349e1821d1b2b05
Dirk Eddelbuettel <edd@debian.org>
2016-06-01 18:31:53 +0200

    some small additions

--- demo/ekf_sine_demo.R
+++ demo/ekf_sine_demo.R
@@ -17,6 +17,74 @@
 
 suppressMessages(library(RcppKalman))
 
+ekf_sine_f <- function(x, param) {
+    ##% Dynamical model function for the random sine signal demo
+
+    ##% Copyright (C) 2007 Jouni Hartikainen
+    ##%
+    ##% This software is distributed under the GNU General Public 
+    ##% Licence (version 2 or later); please refer to the file 
+    ##% Licence.txt, included with the software, for details.
+
+    ##function x_n = ekf_sine_f(x,param)
+    ##dt = param(1);
+    ##A = [1 dt 0;0 1 0;0 0 1];
+    ##x_n = A*x(1:3,:);
+    ##if size(x,1) == 7 || size(x,1) == 6
+    ##    x_n(1:3,:) = x_n(1:3,:) + x(4:6,:);
+    ##end
+    dt <- param[1]
+    A <- matrix(c(1, dt, 0,
+                  0,  1, 0,
+                  0,  0, 1), nrow=3, byrow=TRUE)
+    x_n <- A %*% x[1:3, ]
+    if (nrow(x) == 7 || nrow(x) == 6) {
+        x_n[1:3, ] <- x_n[1:3,] + x <- n[4:6,]
+    }
+    x_n
+}
+
+ekf_sine_h <- function(x, param) {
+    ##% Measurement model function for the random sine signal demo
+
+    ##% Copyright (C) 2007 Jouni Hartikainen
+    ##%
+    ##% This software is distributed under the GNU General Public 
+    ##% Licence (version 2 or later); please refer to the file 
+    ##% Licence.txt, included with the software, for details.
+
+    ##function Y = ekf_sine_h(x,param)
+    ##f = x(1,:);
+    ##a = x(3,:);
+
+    ##Y = a.*sin(f);
+    ##if size(x,1) == 7
+    ##  Y = Y + x(7,:);
+    ##end
+    f <- x[1, ]
+    a <- x[3, ]
+    Y <- a * sin(f)
+    if (nrow(x) == 7) {
+        Y <- Y + x[7,]
+    }
+    Y
+}
+
 ekf_sine_demo <- function() {
 
+    ##clc;
+    ##disp('Filtering the signal with EKF...');
+
+    ##save_plots = 1;
+
+    ##% Measurement model and it's derivative
+    ##f_func = @ekf_sine_f;
+    ##h_func = @ekf_sine_h;
+    ##dh_dx_func = @ekf_sine_dh_dx;
+    ##d2h_dx2_func = @ekf_sine_d2h_dx2;
+    f_func <- ekf_sine_f
+    h_func <- ekf_sine_h
+
+    invisible(NULL)
 }
+
